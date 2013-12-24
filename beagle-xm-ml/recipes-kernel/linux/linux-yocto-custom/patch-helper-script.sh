cfd65357f10ab0d1f21d31c28121949893792fa8
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2013-12-24 22:31:26 +0200

    attempt to apply all patches

--- beagle-xm-ml/recipes-kernel/linux/linux-yocto-custom/patch-helper-script.sh
+++ beagle-xm-ml/recipes-kernel/linux/linux-yocto-custom/patch-helper-script.sh
@@ -1,6 +1,8 @@
 find . -name \*.patch | sort -n > intermediate_file
 cat intermediate_file | sed 's/^..//' > intermediate_file_1
-rm -f intermediate_file
-awk '{print "patch " $0}' intermediate_file_1 > proposed_beagle-xm-ml-user-patches.scc
-rm -f intermediate_file_1
-echo "update your beagle-xm-ml-user-patches.scc from proposed_beagle-xm-ml-user-patches.scc"
+rm -f intermediate_file  
+awk '{print "file://" $0}' intermediate_file_1 > intermediate_file_2
+awk '{print $0" \\"}' intermediate_file_2 > add_to_linux_recipe.txt
+rm -f intermediate_file_1  
+rm -f intermediate_file_2
+echo "update your linux-yocto-custom.bb with add_to_linux_recipe.txt"  
