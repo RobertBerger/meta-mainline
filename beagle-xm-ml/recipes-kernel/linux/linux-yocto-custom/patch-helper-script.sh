6eeac9ba9f76f8a8a40fdfbeabbeb279e70cda97
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2013-12-25 13:22:51 +0200

    attempt to apply all patches

--- beagle-xm-ml/recipes-kernel/linux/linux-yocto-custom/patch-helper-script.sh
+++ beagle-xm-ml/recipes-kernel/linux/linux-yocto-custom/patch-helper-script.sh
@@ -1,8 +1,25 @@
-find . -name \*.patch | sort -n > intermediate_file
-cat intermediate_file | sed 's/^..//' > intermediate_file_1
-rm -f intermediate_file  
-awk '{print "file://" $0}' intermediate_file_1 > intermediate_file_2
-awk '{print $0" \\"}' intermediate_file_2 > add_to_linux_recipe.txt
-rm -f intermediate_file_1  
-rm -f intermediate_file_2
-echo "update your linux-yocto-custom.bb with add_to_linux_recipe.txt"  
+set -e
+
+# don't use builtin 'echo' from /bin/sh
+export ECHO="$(which echo)"
+
+PATCHPATH="patches"
+
+# It is essential to apply the patches in the right order !
+PATCHSET="omap-next-dt dma-devel general-fixes dtc-fixes dtc-overlays of-fixes pdev-fixes mmc-fixes dts-fixes i2c-fixes pinctrl-fixes capemgr reset capes lcdc-fixes net deassert-hard-reset cape-import audio drm cpufreq"
+
+OUTPUT_FILE="proposed_beagle-xm-ml-user-patches.scc"
+
+rm -f ${OUTPUT_FILE}
+
+for patchset in ${PATCHSET} ; do
+   for patch in $(ls -1 ${PATCHPATH}/$patchset/*.patch | sort -n) ; do
+           $ECHO -e "$patch"
+           $ECHO -e "patch $patch" >> ${OUTPUT_FILE}
+#           read r
+   done
+done
+
+echo
+echo
+echo ">>>>>> update your beagle-xm-ml-user-patches.scc file <<<<<<"
