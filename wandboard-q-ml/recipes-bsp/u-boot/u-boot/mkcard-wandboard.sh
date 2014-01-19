eda284e3807e78d21524d074037379daf28802a8
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-01-19 12:17:20 +0200

    more verbosity

--- wandboard-q-ml/recipes-bsp/u-boot/u-boot/mkcard-wandboard.sh
+++ wandboard-q-ml/recipes-bsp/u-boot/u-boot/mkcard-wandboard.sh
@@ -16,7 +16,7 @@
 export LC_ALL=C
 
 if [ $# -ne 2 ]; then
-        echo "Usage: $0 <drive> <full path to u-boot.imx>"
+        echo "+ Usage: $0 <drive> <full path to u-boot.imx>"
         exit 1;
 fi
 
@@ -26,15 +26,16 @@ DRIVE=$1
 U_BOOT_IMX=$2
 
 if [ -f ${U_BOOT_IMX} ]; then
-    echo "${U_BOOT_IMX} exists - I'll dd it to ${DRIVE}"
-    echo "press <ENTER> to go on"
+    echo "+ ${U_BOOT_IMX} exists - I'll dd it to ${DRIVE}"
+    echo "+ press <ENTER> to go on"
     read r
 else
-    echo "${U_BOOT_IMX} does not exist"
+    echo "+ ${U_BOOT_IMX} does not exist"
     exit
 fi
 
 # Erase microSD/SD card:
+echo "+ sudo dd if=/dev/zero of=${DRIVE} bs=1M count=16"
 sudo dd if=/dev/zero of=${DRIVE} bs=1M count=16
 
 # Install Bootloader:
@@ -44,13 +45,20 @@ sudo dd if=${U_BOOT_IMX} of=${DRIVE} bs=512 seek=2
 echo "+ sync"
 sync
 
+echo "+ # 0xE --> \"Win95 FAT16 (LBA)\""
+echo "+ {"
+echo "+ echo 1,48,0xE,*"
+echo "+ echo ,,,-"
+echo "+ } | sudo sfdisk --in-order --Linux --unit M ${DRIVE}"
+
 # 0xE --> "Win95 FAT16 (LBA)"
 {
 echo 1,48,0xE,*
 echo ,,,-
 } | sudo sfdisk --in-order --Linux --unit M ${DRIVE}
 
-sleep 1
+echo "+ sync"
+sync
 
 if [ -b ${DRIVE}1 ]; then
        echo "+ sudo umount -f ${DRIVE}1"
@@ -64,7 +72,7 @@ else
                 echo "+ sudo mkfs.vfat -F 16 -n "boot" ${DRIVE}p1"
                 sudo mkfs.vfat -F 16 -n "boot" ${DRIVE}p1
         else
-                echo "Cant find boot partition in /dev"
+                echo "+ Cant find boot partition in /dev"
         fi
 fi
 
@@ -80,7 +88,7 @@ else
                 echo "+ sudo mkfs.ext3 -L "rootfs" ${DRIVE}p2"
                 sudo mkfs.ext3 -L "rootfs" ${DRIVE}p2
         else
-                echo "Cant find rootfs partition in /dev"
+                echo "+ Cant find rootfs partition in /dev"
         fi
 fi
 
--- wandboard-q-ml/recipes-bsp/u-boot/u-boot/mmc-core-image-minimal-dev-wandboard.sh
+++ wandboard-q-ml/recipes-bsp/u-boot/u-boot/mmc-core-image-minimal-dev-wandboard.sh
@@ -3,7 +3,7 @@
 export LC_ALL=C
 
 if [ $# -ne 6 ]; then
-        echo "Usage: $0 <boot_part> <kernel_file> <fdt> <rootfs_part> <rootfs_file.tar.bz2> <kernel_modules_file.tar.gz>"
+        echo "+ Usage: $0 <boot_part> <kernel_file> <fdt> <rootfs_part> <rootfs_file.tar.bz2> <kernel_modules_file.tar.gz>"
         exit 1;
 fi
 
@@ -13,12 +13,12 @@ fi
 BOOT_PART=$1
 
 if [ -d ${BOOT_PART} ]; then
-    echo "${BOOT_PART} exists"
-    echo "press <ENTER> to go on"
+    echo "+ ${BOOT_PART} exists"
+    echo "+ press <ENTER> to go on"
     read r
 else
-    echo "${BOOT_PART} does not exist"
-    echo "is this partition mounted?"
+    echo "+ ${BOOT_PART} does not exist"
+    echo "+ is this partition mounted?"
     exit
 fi
 
@@ -26,11 +26,11 @@ fi
 KERNEL=$2
 
 if [ -f ${KERNEL} ]; then
-    echo "${KERNEL} exists - I'll cp it to ${BOOT_PART}"
-    echo "press <ENTER> to go on"
+    echo "+ ${KERNEL} exists - I'll cp it to ${BOOT_PART}"
+    echo "+ press <ENTER> to go on"
     read r
 else
-    echo "${KERNEL} does not exist"
+    echo "+ ${KERNEL} does not exist"
     exit
 fi
 
@@ -41,11 +41,11 @@ cp ${KERNEL} ${BOOT_PART}
 FDT=$3
 
 if [ -f ${FDT} ]; then
-    echo "${FDT} exists - I'll cp it to ${BOOT_PART}"
-    echo "press <ENTER> to go on"
+    echo "+ ${FDT} exists - I'll cp it to ${BOOT_PART}"
+    echo "+ press <ENTER> to go on"
     read r
 else
-    echo "${FDT} does not exist"
+    echo "+ ${FDT} does not exist"
     exit
 fi
 
@@ -56,12 +56,12 @@ cp ${FDT} ${BOOT_PART}
 ROOTFS_PART=$4
 
 if [ -d ${ROOTFS_PART} ]; then
-    echo "${ROOTFS_PART} exists"
-    echo "press <ENTER> to go on"
+    echo "+ ${ROOTFS_PART} exists"
+    echo "+ press <ENTER> to go on"
     read r
 else
-    echo "${ROOTFS_PART} does not exist"
-    echo "is this partition mounted?"
+    echo "+ ${ROOTFS_PART} does not exist"
+    echo "+ is this partition mounted?"
     exit
 fi
 
@@ -69,12 +69,12 @@ fi
 ROOTFS_FILE=$5
 
 if [ -f ${ROOTFS_FILE} ]; then
-    echo "${ROOTFS_FILE} exists - I'll unpack it to ${ROOTFS_PART}"
-    echo "needs to be .tar.bz2!"
-    echo "press <ENTER> to go on"
+    echo "+ ${ROOTFS_FILE} exists - I'll unpack it to ${ROOTFS_PART}"
+    echo "+ needs to be .tar.bz2!"
+    echo "+ press <ENTER> to go on"
     read r
 else
-    echo "${ROOTFS_FILE} does not exist"
+    echo "+ ${ROOTFS_FILE} does not exist"
     exit
 fi
 
@@ -85,12 +85,12 @@ sudo tar xjvf ${ROOTFS_FILE} -C ${ROOTFS_PART}
 KERNEL_MODULES_FILE=$6
 
 if [ -f ${KERNEL_MODULES_FILE} ]; then
-    echo "${KERNEL_MODULES_FILE} exists - I'll unpack it to ${ROOTFS_PART}"
-    echo "needs to be .tar.gz!"
-    echo "press <ENTER> to go on"
+    echo "+ ${KERNEL_MODULES_FILE} exists - I'll unpack it to ${ROOTFS_PART}"
+    echo "+ needs to be .tar.gz!"
+    echo "+ press <ENTER> to go on"
     read r
 else
-    echo "${KERNEL_MODULES_FILE} does not exist"
+    echo "+ ${KERNEL_MODULES_FILE} does not exist"
     exit
 fi
 
@@ -98,8 +98,8 @@ echo "+ sudo tar xvf ${KERNEL_MODULES_FILE} -C ${ROOTFS_PART}"
 sudo tar xvf ${KERNEL_MODULES_FILE} -C ${ROOTFS_PART}
 
 # uEnv.txt
-echo "+ cp uEnv-wandboard.txt ${BOOT_PART}/uEnv.txt"
-cp uEnv-wandboard.txt ${BOOT_PART}/uEnv.txt
+echo "+ cp mmc-uEnv-wandboard.txt ${BOOT_PART}/uEnv.txt"
+cp mmc-uEnv-wandboard.txt ${BOOT_PART}/uEnv.txt
 
 echo "+sync"
 sync
@@ -110,5 +110,5 @@ ls -la ${BOOT_PART}
 echo "+ ls -la ${ROOTFS_PART}"
 ls -la ${ROOTFS_PART}
 
-echo "issue this command to umount the paritions:"
-echo "umount ${BOOT_PART} ${ROOTFS_PART}"
+echo "+ issue this command to umount the partitions:"
+echo "+ umount ${BOOT_PART} ${ROOTFS_PART}"
