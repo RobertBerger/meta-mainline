d4cdb2f29e8cf7ce1f5221435331d44e61c464c9
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-08-14 11:03:58 +0300

    removed some stuff

--- scripts/for-contributors/fetch-merge-upstream.sh
+++
@@ -1,6 +0,0 @@
-echo "make sure you are on your dora-armv7-multiplatform-v3.13.x_LOCAL branch"
-echo "press <ENTER> to go on"
-read r
-git fetch upstream
-git merge upstream/dora-armv7-multiplatform-v3.13.x
-
--- wandboard-q-ml/recipes-bsp/u-boot/files/readme.txt
+++
@@ -1,7 +0,0 @@
------------
-u-boot:
-
-image: Allow images to indicate they're loadable at any address:
-http://permalink.gmane.org/gmane.comp.boot-loaders.u-boot/113820
------------
-
