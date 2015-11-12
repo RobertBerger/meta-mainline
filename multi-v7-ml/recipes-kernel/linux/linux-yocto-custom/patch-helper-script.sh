5ffa8685791c14c04b06b5deeb14b258702ede1b
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2015-11-12 20:47:45 +0200

    multi-v7 support

---
+++ multi-v7-ml/recipes-kernel/linux/linux-yocto-custom/patch-helper-script.sh
@@ -0,0 +1,25 @@
+set -e
+
+# don't use builtin 'echo' from /bin/sh
+export ECHO="$(which echo)"
+
+PATCHPATH="patches"
+
+# It is essential to apply the patches in the right order !
+PATCHSET="dts fixes usb dts-bone dts-bone-capes static-capes saucy"
+
+OUTPUT_FILE="proposed_multi-v7-ml-user-patches.scc"
+
+rm -f ${OUTPUT_FILE}
+
+for patchset in ${PATCHSET} ; do
+   for patch in $(ls -1 ${PATCHPATH}/$patchset/*.patch | sort -n) ; do
+           $ECHO -e "$patch"
+           $ECHO -e "patch $patch" >> ${OUTPUT_FILE}
+#           read r
+   done
+done
+
+echo
+echo
+echo ">>>>>> update your multi-v7-ml-user-patches.scc file <<<<<<"
